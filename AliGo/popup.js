78d133a385f5144d4cc92432bbaec4a5d7314487
马田园 <49979803+nnsmty@users.noreply.github.com>
2019-11-09 10:38:02 +0800

    Revert "I changed the code"
    This reverts commit 1c5ccbfc2d89e3df78fbf9e03fbcaaa530fe7d99.

--- AliGo/popup.js
+++ AliGo/popup.js
@@ -16,182 +16,179 @@
 			   
 			   
 
-//var url = window.location.host;
-//if (url == 'www.aliexpress.com'){
-    function drawTable(data) {
+function drawTable(data) {
+	
 
-
-        for (var i = 0; i < data.length; i++) {
-            drawRow(data[i]);
-        }
+    for (var i = 0; i < data.length; i++) {
+        drawRow(data[i]);
     }
+}
 
-    function drawRow(rowData) {
-        var row = $("<tr />")
-        $("#personDataTable").append(row);
-        row.append($("<td>" + rowData.id + "</td>"));
-        row.append($("<td>" + rowData.title + "</td>"));
-        row.append($("<td>" + rowData.userId + "</td>"));
-    }
-
-
-    var current_url="";
-
-    chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
-        console.log("working1 " + tabs[0].url);
-
-         current_url= tabs[0].url ;
-
-         check_aliexpress_product (current_url);
-         setlink();
-    });
-
-    function setlink() {
+function drawRow(rowData) {
+    var row = $("<tr />")
+    $("#personDataTable").append(row); 
+    row.append($("<td>" + rowData.id + "</td>"));
+    row.append($("<td>" + rowData.title + "</td>"));
+    row.append($("<td>" + rowData.userId + "</td>"));
+}
 
 
+var current_url="";
 
-    //$("#link").text(current_url);
-    $("#link").attr("href", current_url);
-    split_to_get_id(current_url);
+chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
+    console.log("working1 " + tabs[0].url);
 
+	 current_url= tabs[0].url ;
 
+	 check_aliexpress_product (current_url);
+	 setlink();
+});
 
-    }
+function setlink() {
+	
 
-    var url_to_id ;
-    function split_to_get_id(current_url) {
-
-        /* var pathArray = current_url.split('.html');
-
-        var secondLevelLocation = pathArray[0] ;
-        secondLevelLocation =pathArray.split('/');
-        secondLevelLocation = pathArray[0] */ ;
-        var url = current_url;
-        url_to_id = getPageName(url);
-
-
-    $("#product_id").text(url_to_id );
-    get_prod_if(url_to_id);
-    }
-
-
-    //spliting the url to get product id
-    function getPageName(url) {
-        var index = url.lastIndexOf("/") + 1;
-        var filenameWithExtension = url.substr(index);
-        var filename = filenameWithExtension.split(".")[0];
-        return filename;
-    }
-
-    function check_aliexpress_product (current_url){
-
-            var pathArray = current_url.split('/');
-
-        var secondLevelLocation = pathArray[3] ;
-        //console.log(secondLevelLocation) ;
-        if (secondLevelLocation=="item"){
-
-                console.log("This is Ali Exprees Item") ;
-                console.log("Its not a Aliexpress Product ") ;
-                $("#header_text").text("Ali Express Product" );
-                $("#header_id").css('background', 'Green');
-
-        }else{
-
-                    console.log("Its not a Aliexpress Product ") ;
-                    $("#header_text").text("No Ali Express Product" );
-                    $("#header_id").css('background', 'red');
-
-                    $( "#all_data" ).hide();
+	
+//$("#link").text(current_url);
+$("#link").attr("href", current_url);
+split_to_get_id(current_url);
 
 
 
-        }
+}
+
+var url_to_id ;
+function split_to_get_id(current_url) {
+	
+	/* var pathArray = current_url.split('.html');	
+	
+	var secondLevelLocation = pathArray[0] ;
+	secondLevelLocation =pathArray.split('/');
+	secondLevelLocation = pathArray[0] */ ;
+	var url = current_url;
+	url_to_id = getPageName(url);
+
+
+$("#product_id").text(url_to_id );
+get_prod_if(url_to_id);
+}
+
+
+//spliting the url to get product id 
+function getPageName(url) {
+    var index = url.lastIndexOf("/") + 1;
+    var filenameWithExtension = url.substr(index);
+    var filename = filenameWithExtension.split(".")[0];
+    return filename;                                    
+}
+
+function check_aliexpress_product (current_url){ 
+	
+		var pathArray = current_url.split('/');	
+	
+	var secondLevelLocation = pathArray[3] ; 
+	//console.log(secondLevelLocation) ;
+	if (secondLevelLocation=="item"){
+		
+			console.log("This is Ali Exprees Item") ;
+			console.log("Its not a Aliexpress Product ") ;
+			$("#header_text").text("Ali Express Product" );
+			$("#header_id").css('background', 'Green');
+			
+	}else{
+		
+		 		console.log("Its not a Aliexpress Product ") ;
+				$("#header_text").text("No Ali Express Product" );
+				$("#header_id").css('background', 'red');
+				
+				$( "#all_data" ).hide();
+		
+
+		
+	}
+	
+	
+	
+}
+$( "#pro_i" ).click(function() {
+pro();
+});
+
+
+function pro(){
+//    var id = $('#product_id').html()
+//	alert(id)
+		$("#header_text").text("Importing..." );
+			$("#header_id").css('background', '#FF4747');
+	
+	
+	  $.ajax({
+		  type: "POST",//传输方式
+		  url: "https://140.82.35.203/Traversy/",//url
+		  data: {"id":url_to_id},//传输的数据
+		  success: function (data) {
+			  if(data=='have'){
+				 // alert('We already have its data!')
+				 
+				  $("#header_text").text("Already Imported " );
+					$("#header_id").css('background', 'Red');
+			  }else if(data == 'error'){
+				  alert('Unknown error!')
+			  }else{
+				 // alert('Save Succeeded！')
+				 $("#header_text").text("Successfully Imported " );
+				$("#header_id").css('background', 'Green');
+	
+			  }
+		  },
+		  //防止深度序列化。传递数组元素属性必设置为true！
+		  traditional: true,
+		  error: function (data) {
+			  alert(data);
+		  }
+		});
+	
+	
+	
 
 
 
-    }
-    $( "#pro_i" ).click(function() {
-    pro();
-    });
+}
 
-
-    function pro(){
-    //    var id = $('#product_id').html()
-    //	alert(id)
-            $("#header_text").text("Importing..." );
-                $("#header_id").css('background', '#FF4747');
-
-
-          $.ajax({
-              type: "POST",//传输方式
-              url: "https://140.82.35.203/Traversy/",//url
-              data: {"id":url_to_id},//传输的数据
-              success: function (data) {
-                  if(data=='have'){
-                     // alert('We already have its data!')
-
-                      $("#header_text").text("Already Imported " );
-                        $("#header_id").css('background', 'Red');
-                  }else if(data == 'error'){
-                      alert('Unknown error!')
-                  }else{
-                     // alert('Save Succeeded！')
-                     $("#header_text").text("Successfully Imported " );
-                    $("#header_id").css('background', 'Green');
-
-                  }
-              },
-              //防止深度序列化。传递数组元素属性必设置为true！
-              traditional: true,
-              error: function (data) {
-                  alert(data);
-              }
+function get_prod_if(url_to_id){
+//get_product_in
+	   $(document).ready(function() {
+	        $.ajax({
+                type:'get',
+                url:'https://140.82.35.203/json/api/'+url_to_id,
+                success:function(data){
+                    $('#stage').html('<p> Product Name : ' + data['product_Name'] + '</p>');
+                    $('#stage').append('<p>Orders: ' + data['orders']+ '</p>');
+                    $('#stage').append('<p> Reviews: ' + data['reviews']+ '</p>');
+                    $('#stage').append('<p> Overview Rating: ' + data['overview-rating']+ '</p>');
+                    $('#stage').append('<p> Stock: ' + data['Stock']+ '</p>');
+                    $('#stage').append('<p> Tags: ' + data['freight']['*Tags']+ '</p>');
+                    $('#stage').append('<p> Description: ' + data['freight']['Description']+ '</p>');
+                    $('#stage').append('<p> Freight: ' + data['freight']['Declared Name']+ '</p>');
+                    $('#stage').append('<p> Declared Local Name: ' + data['freight']['Declared Local Name']+ '</p>');
+                    $('#stage').append('<p> Localized Shipping: ' + data['freight']['*Shipping']+ '</p>');
+                    $('#stage').append('<p> Country Of Origin: ' + data['freight']['Country Of Origin']+ '</p>');
+                    $('#stage').append('<p> StoreName: ' + data['storeName']+ '</p>');
+                    $('#stage').append('<p> Followers: ' + data['followers']+ '</p>');
+                    $('#stage').append('<p> Rating: ' + data['rating']+ '</p>');
+                },
+                error:function(){
+                    alert("Request Error");
+                },
+                dataType:"json"
             });
-
-
-
-
-
-
-    }
-
-    function get_prod_if(url_to_id){
-    //get_product_in
-           $(document).ready(function() {
-                $.ajax({
-                    type:'get',
-                    url:'https://140.82.35.203/json/api/'+url_to_id,
-                    success:function(data){
-                        $('#stage').html('<p> Product Name : ' + data['product_Name'] + '</p>');
-                        $('#stage').append('<p>Orders: ' + data['orders']+ '</p>');
-                        $('#stage').append('<p> Reviews: ' + data['reviews']+ '</p>');
-                        $('#stage').append('<p> Overview Rating: ' + data['overview-rating']+ '</p>');
-                        $('#stage').append('<p> Stock: ' + data['Stock']+ '</p>');
-                        $('#stage').append('<p> Tags: ' + data['freight']['*Tags']+ '</p>');
-                        $('#stage').append('<p> Description: ' + data['freight']['Description']+ '</p>');
-                        $('#stage').append('<p> Freight: ' + data['freight']['Declared Name']+ '</p>');
-                        $('#stage').append('<p> Declared Local Name: ' + data['freight']['Declared Local Name']+ '</p>');
-                        $('#stage').append('<p> Localized Shipping: ' + data['freight']['*Shipping']+ '</p>');
-                        $('#stage').append('<p> Country Of Origin: ' + data['freight']['Country Of Origin']+ '</p>');
-                        $('#stage').append('<p> StoreName: ' + data['storeName']+ '</p>');
-                        $('#stage').append('<p> Followers: ' + data['followers']+ '</p>');
-                        $('#stage').append('<p> Rating: ' + data['rating']+ '</p>');
-                    },
-                    error:function(){
-                        alert("Request Error");
-                    },
-                    dataType:"json"
-                });
-    //               $.getJSON("https://140.82.35.203/static/download/Json/"+url_to_id+".json", function(jd) {
-    //                  $('#stage').html('<p> Product Name : ' + jd.Stock + '</p>');
-    //                  $('#stage').append('<p>Orders: ' + jd.orders+ '</p>');
-    //                  $('#stage').append('<p> reviews: ' + jd.reviews+ '</p>');
-    //               });
-
-
-             });
-
-    //		 alert("https://140.82.35.203/static/download/Json/"+url_to_id+".json");
-    }
-//}
+//               $.getJSON("https://140.82.35.203/static/download/Json/"+url_to_id+".json", function(jd) {
+//                  $('#stage').html('<p> Product Name : ' + jd.Stock + '</p>');
+//                  $('#stage').append('<p>Orders: ' + jd.orders+ '</p>');
+//                  $('#stage').append('<p> reviews: ' + jd.reviews+ '</p>');
+//               });
+            
+               
+         });
+		 
+//		 alert("https://140.82.35.203/static/download/Json/"+url_to_id+".json");
+}
--- AliGo/single_product.js
+++ AliGo/single_product.js
@@ -20,54 +20,51 @@ s.onload = function() {
  */
 
 
-var url = window.location.host;
-if (url == 'www.aliexpress.com'){
-    var url_im = chrome.runtime.getURL('icon_68.png') ;
+var url_im = chrome.runtime.getURL('icon_68.png') ;
 
-    var url=window.location.href;
-    var arr=url.split('.html')[0];
-    var pro_id=arr.split('/')[4];
-    var check_link_=arr.split('/')[3];
+var url=window.location.href;
+var arr=url.split('.html')[0];
+var pro_id=arr.split('/')[4];
+var check_link_=arr.split('/')[3];
 
 
 
 
-    if(check_link_=="item"){
+if(check_link_=="item"){
 
-    $( "body" ).append( $( '<a id="import_product_bottom" title="Push this product to AliGo" class="Go-fob"><img src='+url_im+' alt="Import product to AliGo"></a>') );
-
-
-
-    $( "#import_product_bottom" ).on( "click", function() {
-
-
-                                if (confirm('Are you sure to import this item?')){
-                      $.ajax({
-                          type: "POST",//传输方式
-                          url: "https://140.82.35.203/Traversy/",//url
-                          data: {"id":pro_id},//传输的数据
-                          success: function (data) {
-                              if(data=='have'){
-                                  alert('We already have its data!')
-                              }else if(data == 'error'){
-                                  alert('Unknown error!')
-                              }else{
-                                  alert('Save Succeeded！')
-                              }
-                          },
-                          //防止深度序列化。传递数组元素属性必设置为true！
-                          traditional: true,
-                          error: function (data) {
-                              alert(data);
-                          }
-                        });
-                    }
+$( "body" ).append( $( '<a id="import_product_bottom" title="Push this product to AliGo" class="Go-fob"><img src='+url_im+' alt="Import product to AliGo"></a>') );
 
 
 
+$( "#import_product_bottom" ).on( "click", function() {
 
 
-    });
+							if (confirm('Are you sure to import this item?')){
+				  $.ajax({
+					  type: "POST",//传输方式
+					  url: "https://140.82.35.203/Traversy/",//url
+					  data: {"id":pro_id},//传输的数据
+					  success: function (data) {
+						  if(data=='have'){
+							  alert('We already have its data!')
+						  }else if(data == 'error'){
+							  alert('Unknown error!')
+						  }else{
+							  alert('Save Succeeded！')
+						  }
+					  },
+					  //防止深度序列化。传递数组元素属性必设置为true！
+					  traditional: true,
+					  error: function (data) {
+						  alert(data);
+					  }
+					});
+				}
+	
+	
+	
+	
+	
+});
 
-    }
 }
